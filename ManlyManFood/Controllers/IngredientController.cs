548bf569cd8261cb723fa38d4a709419fa9fd2eb
Yoeri Van Damme <yoeri.vandamme@gmail.com>
2015-03-09 11:22:50 +0100

    create ingredients controller

---
+++ ManlyManFood/Controllers/IngredientController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ManlyManFood.DataProvider;
+using ManlyManFood.Models;
+
+namespace ManlyManFood.Controllers
+{
+	[RoutePrefix("ingredients")]
+	public class IngredientController : ApiController
+	{
+		private readonly IngredientsProvider _ingredientsProvider;
+
+		public IngredientController()
+		{
+			_ingredientsProvider = new IngredientsProvider();
+		}
+
+		//GET
+		[Route("")]
+		public IEnumerable<Ingredient> Get()
+		{
+			return _ingredientsProvider.Ingredients;
+		}
+		[Route("{id}")]
+		public Ingredient Get(int id)
+		{
+			return _ingredientsProvider.Ingredients.Single(rec => rec.Id == id);
+		}
+	}
+}
