ec236510f28bbdfc40c2c4898fe887006419826e
Yoeri Van Damme <yoeri.vandamme@gmail.com>
2015-03-10 09:22:03 +0100

    added validation

--- ManlyManFood/Models/Recipe.cs
+++ ManlyManFood/Models/Recipe.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ManlyManFood.Models
 {
 	public class Recipe
 	{
 		public int Id { get; set; }
+		[Required]
+		[MinLength(3)]
 		public string Name { get; set; }
 		public IEnumerable<Ingredient> Ingredients { get; set; } 
 	}
